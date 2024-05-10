418c48cd20a613cc34dbe438e9155ffd5dd560f6
kr77295 <113361356+kr77295@users.noreply.github.com>
2024-05-10 07:20:56 +0000

    datatypes and variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 144553;
+let accountEmail = "vkr69511@gmail.com";
+var accountPassword = "12345";
+accountCity = "Jaipur";
+let acountState;
+
+//accountId = 2 // not allowed
+
+/* prefer not to  use var because the issue in block scpe and functional scope */
+accountPassword = "2121213"
+accountEmail = "krvinay61@gmail.com";
+accountCity = "bellary";
+console.table([accountId,
+    accountEmail,
+    accountPassword,
+    accountCity,
+    acountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"Use strict"; // treat all js code as new version
+
+//alert(3+3)  //we are uisng node js not the browser
+
+
+// number => 2 to power 53
+//bigint
+//string
+//boolean
+//null
+//undefined
+//symbol => unique
+let name = "hitesh"
+let age = 18
+let isLoggedIn = true
+let state;
+
+
+
+
+//object
+
+console.log(typeof undefined);// undefined
+console.log(typeof null);// object
