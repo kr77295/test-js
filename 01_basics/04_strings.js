470ffbb44e42b538bc69afd0324c54afbb30d6f4
kr77295 <113361356+kr77295@users.noreply.github.com>
2024-05-14 11:14:15 +0000

    strings

---
+++ 01_basics/04_strings.js
@@ -0,0 +1,46 @@
+const name = "hitesh"
+const repoCount = 50;
+
+// console.log(name + repoCount + " value");
+console.log(`Hello, my name is ${name} and my repo count is ${repoCount}`);
+
+
+const gameName = new String("vinay-kr-ramu")
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));//knowing at  index which alphabet is present
+console.log(gameName.indexOf("v"));//knowing the index number of the alphabet
+ 
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+
+const anotherString = gameName.slice(-4,2)
+console.log(anotherString);
+
+const newStringOne = "    hitesh    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://vinay.com/vinay%20kulkarni";
+
+console.log(url.replace('%20','-'));
+
+console.log(url.includes('hitesh'));
+
+console.log(gameName.split('-'));
+
+
+
+
+
+ 
+
+
+
