eb37a3a8ba733297e5dcb2562008bda599cacb2b
kr77295 <113361356+kr77295@users.noreply.github.com>
2024-05-14 07:31:08 +0000

    stacks and heap

--- dataTypesSummary.js
+++ dataTypesSummary.js
@@ -25,16 +25,39 @@ let myObj = {
     age: 26
 }
 const myFunction = function () {
-    console.log("Hello world");
+    // console.log("Hello world");
 }
- console.log(typeof Score ); // number
- console.log(typeof scoreValue );// number
- console.log(typeof isLoggedIn );// boolean
- console.log(typeof outsideTemperature );//object
- console.log(typeof userEmail );//undefined
- console.log(typeof heros );//object
- console.log(typeof myObj );//object
- console.log(id === anotherId );//false
- console.log(id == anotherId );//false
+//  console.log(typeof Score ); // number
+//  console.log(typeof scoreValue );// number
+//  console.log(typeof isLoggedIn );// boolean
+//  console.log(typeof outsideTemperature );//object
+//  console.log(typeof userEmail );//undefined
+//  console.log(typeof heros );//object
+//  console.log(typeof myObj );//object
+//  console.log(id === anotherId );//false
+//  console.log(id == anotherId );//false
+
+
+ //stack (primitve) and heap(non-primitive) Memory
+
+
+ let myYoutubeName = "vinaydotcom";
+
+ let anotherName = myYoutubeName;
+ anotherName = "vinay"
+
+ console.log(myYoutubeName);
+ console.log(anotherName);
+
+ let userOne = {
+    email:"user@google.com",
+    upi:"user@ybl"
+ }
+ let userTwo = userOne;
+
+ userTwo.email = "krvinay@google.com"
+
+ console.log(userOne.email);//krvinay@google.com
+ console.log(userTwo.email);//krvinay@google.com
 
 
